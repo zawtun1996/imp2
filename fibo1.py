00fdcea8345993781cb1c86c34a527261e6b5f47
zawtun1996 <ztun4017@gmail.com>
2019-09-13 18:41:26 +0630

    first commit

---
+++ fibo1.py
@@ -0,0 +1,16 @@
+#import fibo
+
+#f = fibo.fib 
+#f(100)
+
+#fibo.fib(10000)
+
+#from fibo import fib, fib2
+#
+
+
+#from fibo import fib as fibonacci
+#fibonacci(500)
+
+from fibo import *
+fib(500)
